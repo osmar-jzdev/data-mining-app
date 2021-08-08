d2ed181eae11009fa2c4ecce8fbefa37d03a4cdf
osmar-jzdev <techsmart.dev@gmail.com>
2021-08-07 19:41:24 -0500

    eda tab finished and adding Feadture selection tab

--- app.py
+++ app.py
@@ -29,8 +29,106 @@ app  = dash.Dash(eager_loading=True)
 app.title = "Data Mining APP"
 
 
+def read_file(file):
+    '''
+    Funtion to read some file using pandas library.
+
+    Parameters
+    ----------
+    file : Str
+        Path and file to read and load to the APP
+
+    Returns
+    -------
+    df : Pandas Dataframe 
+        Dataset loaded as dataframe from the file readed. 
+
+    '''
+    if '.csv' in str(file):
+        df = pd.read_csv(file)
+    elif '.xlsx' in str(file):
+        df = pd.read_excel(file)
+    elif '.txt' in str(file):
+        df = pd.read_table(file)
+    
+    return df
+
+def dd_select_dataset():
+    '''
+    Funtion to display the data avaible for the project loaded in the data 
+    directory.
+
+    Returns
+    -------
+    file_list : Lits
+        List of the files loaded by default for this project.
+
+    '''
+    app_path = str(pathlib.Path(__file__).parent.resolve())
+    data_path = app_path + '\\data'
+    
+    file_list = list()
+    for file in os.listdir(data_path):
+        file_list.append({'label':file, 'value':data_path + '\\' + file})
+                 
+    return file_list
+
+
+def missing_null_val():
+    '''
+    Function to build a dataframe with hey general info applying a quick 
+    exploration data 
+
+    Returns
+    -------
+    df_summary : Pandas Dtaframe 
+        Dataframe with the generla info to display as table in EDA module.
+
+    '''
+    df_summary = pd.DataFrame(df.isna().sum(), columns = ['Count Null Values'])
+    df_summary = pd.DataFrame(df.isna().sum(), columns = ['Count Null Values'])
+    df_summary['Nunique values'] = df.nunique().to_list()
+    df_summary['Count Records'] = df.count().to_list()
+    df_summary['Columns'] = df_summary.index.to_list()
+    
+    df_summary = df_summary[['Columns','Count Records',
+                             'Count Null Values','Nunique values']]
+    #print(df_summary)
+
+    return df_summary
+
+def desc_data(type_desc):
+    '''
+    Funtion to return the output dataframe by the describe function.
+    
+    Parameters
+    ----------
+    type_desc : Str
+        Flag to build a describe numeric function or describe object function
+
+
+    Returns
+    -------
+    df_desc : Pandas Dataframe
+        Pandas Dtaframe table with the general info for describe method applied.
+
+    '''
+    df_desc = df.describe(include=type_desc)
+    df_desc.reset_index(inplace=True)
+    return df_desc
+
+
 
 def build_banner():
+    '''
+    Funtion to build the top banner of the Application
+
+    Returns
+    -------
+    Lits
+        List of the HTMl and dash comonents to build this GUI section
+
+    '''
     return html.Div(
         id="banner",
         className="banner",
@@ -59,6 +157,16 @@ def build_banner():
 
 
 def build_tabs():
+    '''
+    Funtion to build the Tabs section displayed by the project
+
+    Returns
+    -------
+    Lits
+        Html and Dash componets to display the Tabs used to display the projects
+        modules.
+
+    '''
     return html.Div(
         id = "tabs",
         className = "tabs",
@@ -83,8 +191,8 @@ def build_tabs():
                         selected_className = "custom-tab--selected",
                     ),
                     dcc.Tab(
-                        id = "Claustering-tab", # id = "upload-file-tab"
-                        label = "Claustering",
+                        id = "Feature-selection-tab", # id = "upload-file-tab"
+                        label = "Feature Selection",
                         value = "tab3",
                         className = "custom-tab",
                         selected_className = "custom-tab--selected",
@@ -95,62 +203,30 @@ def build_tabs():
     )
 
 
-def read_file(file):
-    if '.csv' in str(file):
-        df = pd.read_csv(file)
-    elif '.xlsx' in str(file):
-        df = pd.read_excel(file)
-    elif '.txt' in str(file):
-        df = pd.read_table(file)
-    
-    return df
+def build_eda_charts(df_nan,df_box,df_desc_num):
+    '''
+    Funciton to build the plots and tables for EDA dashboard.
 
+    Parameters
+    ----------
+    df_nan : Pandas Dataframe
+        Dataframe with the null and unique values per column.
+        
+    df_box : Pandas Dataframe
+        Dataframe with the numeric columns for the dataframe to display as box plot.
+        
+    df_desc_num : Pandas Dataframe
+        Dataframe with the describe dataframe function output.
 
-#def start_dm():
-    #df = px.data.tips()
-    #df = read_file('data/Hipoteca.csv')
-#    fig = px.histogram(df, x="total_bill", nbins=20)
-#    fig.show()    
-#    print("")
+    Returns
+    -------
+    list
+        All the figures and html containers to display dashboard.
 
-
-def dd_select_dataset():
-    app_path = str(pathlib.Path(__file__).parent.resolve())
-    data_path = app_path + '\\data'
-    
-    file_list = list()
-    for file in os.listdir(data_path):
-        file_list.append({'label':file, 'value':data_path + '\\' + file})
-                 
-    return file_list
-
-
-def missing_null_val():
-    #obteniendo total de valores nulos en el dataframe
-    df_summary = pd.DataFrame(df.isna().sum(), columns = ['Count Null Values'])
-    df_summary = pd.DataFrame(df.isna().sum(), columns = ['Count Null Values'])
-    #obteniendo valores unicos y total de registros por cada columna
-    df_summary['Nunique values'] = df.nunique().to_list()
-    df_summary['Count Records'] = df.count().to_list()
-    df_summary['Columns'] = df_summary.index.to_list()
-    
-    #ordenando las columnas
-    df_summary = df_summary[['Columns','Count Records',
-                             'Count Null Values','Nunique values']]
-    #print(df_summary)
-
-    return df_summary
-
-
-def build_eda_charts(df_nan,df_box):
+    '''
     return [html.Div(id='eda-dashboard',
                      children=[html.Br(),
-                               #dcc.Graph(id='descripting-table2',
-                               #  figure={
-                               #      'data': [],
-                               #      'layout': go.Layout()
-                               #      }
-                               #  ),
+                               html.Label(" Exploration columns "),
                                dash_table.DataTable(id='missing-null-val-tab',
                                                     data = df_nan.to_dict('records'),
                                                     columns=[{"name": i,"id": i} for i in df_nan.columns],
@@ -167,16 +243,68 @@ def build_eda_charts(df_nan,df_box):
                                                     ),
                                html.Br(),
                                dcc.Graph(id='box-plot',
-                                         figure= px.box(df_box)),
-                               html.Br()
+                                         figure= px.box(df_box, 
+                                                        title='Atypic Values Detection')),
+                               html.Br(),
+                               html.Label(" Describing numeric columns "),
+                               dash_table.DataTable(id='describe-num-tab',
+                                                    data = df_desc_num.to_dict('records'),
+                                                    columns=[{"name": i,"id": i} for i in df_desc_num.columns],
+                                                    style_header={
+                                                        'backgroundColor': 'black',
+                                                        'textAlign': 'center'
+                                                    },
+                                                    style_cell={
+                                                            'backgroundColor': '#1e2130',
+                                                            'color': 'white',
+                                                            'padding': '10px',
+                                                            'textAlign': 'center'
+                                                        },
+                                                    ),
+                               html.Br(),
+                               html.Br(),
                                ]
                      )
           ]
     
+def build_feature_selctn_charts():
+    return [html.Div(id='fs-dashboard',
+                     children=[html.Br(),
+                               html.Label(" Feature Selection "),
+                               dcc.Graph(id='scatter-plot-by-column',
+                                         figure= {}),
+                               html.Br(),
+                               html.Label(" Describing numeric columns "),
+                               dash_table.DataTable(id='describe-num-tab2',
+                                                    data = df.to_dict('records'),
+                                                    columns=[{"name": i,"id": i} for i in df.columns],
+                                                    style_header={
+                                                        'backgroundColor': 'black',
+                                                        'textAlign': 'center'
+                                                    },
+                                                    style_cell={
+                                                            'backgroundColor': '#1e2130',
+                                                            'color': 'white',
+                                                            'padding': '10px',
+                                                            'textAlign': 'center'
+                                                        },
+                                                    ),
+                               html.Br(),
+                               html.Br(),
+                               ]
+                     )
+          ]
     
-
-
 def build_tab1_dropdown_files():
+    '''
+    Function to build and display the dropdown selecton files and update button
+
+    Returns
+    -------
+    list
+        The html, adn dash components to display this selection fiel section.
+
+    '''
     return [html.Div(id="select-menu-data",
                      children=[html.Br(),
                                dcc.Dropdown(
@@ -194,25 +322,33 @@ def build_tab1_dropdown_files():
 
 
 def build_tab2_dash_eda():
+    '''
+    Function to Disyplay the EDA dashboard if the data has been selected.
+
+    Returns
+    -------
+    list
+        The html and dash companents to displayn or not the dashboard.
+
+    '''
     if df.empty !=True:
         df_nan = missing_null_val()
         df_box = df[df.describe().columns.to_list()]
-        #empty = pd.DataFrame()
-        #print(df_nan.to_dict())
-        return build_eda_charts(df_nan, df_box)
+        df_describe_num = desc_data('number')
+        return build_eda_charts(df_nan, df_box, df_describe_num)
     else:
         return [html.Div(id="empty-dev",
                          children=[dcc.Tab(id='wrn-msg-empty-data')])]
 
 
-def build_tab3_dash_claustering():
-    #if df.empty !=True:
+def build_tab3_dash_feature_sel():
+    if df.empty !=True:
         #df_nan = missing_null_val()
         #empty = pd.DataFrame()
         #print(df_nan.to_dict())
-        #return build_claustering_charts(df_nan)
-    #else:
-    return [html.Div(id="empty-dev",
+        return build_feature_selctn_charts()
+    else:
+        return [html.Div(id="empty-dev",
                          children=[dcc.Tab(id='wrn-msg-empty-data')])]
 
 
@@ -237,6 +373,23 @@ app.layout = html.Div(id="big-app-container",
      Input("dropdown-files", "value")],    
 )
 def set_data_selection(clicks,drop_down_value):
+    '''
+    Function to update the data selection in the GUI and the whole APP
+
+    Parameters
+    ----------
+    clicks : Int
+        Number of clicks to the Update button for the file selection tab.
+    
+    drop_down_value : Str
+        File selected by the user for the list of files previiuos loaded .
+
+    Returns
+    -------
+    data_pth : Lits
+        List with the path for the file selected by the user.
+
+    '''
     global df 
     
     if clicks != 0:
@@ -252,12 +405,27 @@ def set_data_selection(clicks,drop_down_value):
     [Input("app-tabs", "value")],
 )
 def render_tab_content(tab_switch):
+    '''
+    Function to update the Tab selection for the APP.
+
+    Parameters
+    ----------
+    tab_switch : Str
+        ID for the dash component of the tab selected by the user.
+
+    Returns
+    -------
+    List
+        Based on the tab slected by the user a function returns building the 
+        dash and html components for the tab selected.
+
+    '''
     if tab_switch == 'tab1':
         return build_tab1_dropdown_files()
     elif tab_switch == 'tab2':
         return build_tab2_dash_eda()
     elif tab_switch == 'tab3':
-        return build_tab3_dash_claustering()
+        return build_tab3_dash_feature_sel()
 
 
 
